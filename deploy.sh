ce6e63cf976e899daa375954d6c1f8fa6546c6d3
Oren Maoz Sharabi <108679419+SilentProcess87@users.noreply.github.com>
2025-07-16 17:10:00 +0300

    fix fix-deployment.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 cd /var/www
 rm -R vulnshop
 git clone https://github.com/SilentProcess87/vulnshop-multi-cloud.git vulnshop
-cp vulnshop.backup1/fix-deployment.sh vulnshop
+
 chmod -R 777 vulnshop
 cd vulnshop 
+chmod +x fix-deployment.sh
 ./fix-deployment.sh
